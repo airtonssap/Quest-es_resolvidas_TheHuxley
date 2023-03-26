3f11b5b914c2b6436370b5c0952c071c04142acb
airtonssap <104590505+airtonssap@users.noreply.github.com>
2023-03-25 21:53:14 -0300

    Update vai_pra_onde.py

--- vai_pra_onde.py
+++ vai_pra_onde.py
@@ -1,27 +1,19 @@
-lista_de_cidades = []
-lista = []
+opcoes = []
 while True:
-    cidade = input('Digite a cidade: ')
-    if cidade == 'Fim':
+    destino = input()
+    destino = destino.upper()
+    if(destino == "FIM"):
+        for opcao in opcoes:
+            if(opcao[2] * 2 <= 300):
+                if(opcao[1] >= maiordistancia):
+                    maiordistancia = opcao[1]
+                    destinoescolhido = opcao[0]      
+                    print(destinoescolhido)
+                    break
+        else:
+            print("SEM DESTINO")
         break
-    dis = float(input(f'Digite a distância de {cidade} até o seu local atual: '))
-    valor = float(input('Valor da passagem: ')) * 2
-    if valor > 300:
-        continue
-    lista.append(cidade)
-    lista.append(dis)
-    lista.append(valor)
-    lista_de_cidades.append(lista.copy())
-    lista.clear()
-
-Escolhida = ''
-maiordis = 0
-if len(lista_de_cidades) > 0:
-    for valor in lista_de_cidades:
-        if valor[1] > maiordis:
-            maiordis = valor[1]
-            Escolhida = valor[0].upper()
-    print(Escolhida)
-else:
-    print('SEM DESTINO')
-    
+    distancia = int(input())
+    passagem = int(input())
+    opcoes.append([destino, distancia, passagem])
+    maiordistancia = distancia
